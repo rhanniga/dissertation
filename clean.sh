a513051f3a47f6f5b96b0a45b86c2e28de6df360
rhanniga <ryan.hannigan@cern.ch>
2023-08-21 14:27:57 -0500

    clean up aux files to keep repo clean

---
+++ clean.sh
@@ -0,0 +1,35 @@
+rm *.aux
+rm *.log
+rm *.out
+rm *.toc
+rm *.run.xml
+rm *.bcf
+rm *.fls 
+rm *.fdb_latexmk
+rm *.synctex.gz
+rm *.blg
+rm *.bbl
+rm *.lot
+rm *.idx
+rm *.lof
+rm *.gz
+rm *.ind
+rm *.ilg
+
+git rm *.aux
+git rm *.log
+git rm *.out
+git rm *.toc
+git rm *.run.xml
+git rm *.bcf
+git rm *.fls 
+git rm *.fdb_latexmk
+git rm *.synctex.gz
+git rm *.blg
+git rm *.bbl
+git rm *.lot
+git rm *.idx
+git rm *.lof
+git rm *.gz
+git rm *.ind
+git rm *.ilg
